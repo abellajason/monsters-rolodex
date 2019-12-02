eaa0f4f525bd3c512d4ac876d00335040524746b
Jason Abella <abellajason20@gmail.com>
2019-12-03 03:16:48 +0800

    Updates

---
+++ precache-manifest.0192520db3d1193149d15847d5b1f2de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47b24ec96f989df3edd3ac9c045e39d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0ad0d520d733342e43ab",
+    "url": "/monsters-rolodex/static/css/main.d5e8257d.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "0ad0d520d733342e43ab",
+    "url": "/monsters-rolodex/static/js/main.5fc834a7.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
